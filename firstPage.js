8389c07caf2d8f1db4578aec87c0fa488a96854d
franklin2oo8 <goldcode65@gmail.com>
2025-10-02 18:51:38 +0100

    Add files via upload

---
+++ firstPage.js
@@ -0,0 +1,3 @@
+function tecno(){
+alert("📱 Tecno Pop 6 Go- Display:  6.52 inches HD+ display -> a resolution  1600 x 720 pixels-   Processor: Octa-core processor    RAM: 1GB  Storage: 16GB internal storage (expandable via microSD card)   Camera: Dual rear camera setup  a 13MP primary sensor and a secondary sensor, 5MP front camera Battery:  4000mAh battery capacity   Operating System: Android 11 Go Edition         Other features: Fingerprint sensor, face unlock, dualSIM support");
+}
